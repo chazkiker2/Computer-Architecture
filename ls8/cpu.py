3ae92c21b477a017f9aaa7cee4414e463d929db7
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-10 22:15:13 -0400

    continue cleaning up code in cpu.py

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,12 +8,23 @@ LDI = 0b10000010
 PRN = 0b01000111
 MUL = 0b10100010
 
+DEFAULT_PROGRAM = [
+    # From print8.ls8
+    0b10000010,  # LDI R0,8
+    0b00000000,
+    0b00001000,
+    0b01000111,  # PRN R0
+    0b00000000,
+    0b00000001,  # HLT
+]
+
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
+
         self.ram = [0 for _ in range(256)]
         self.reg = [0 for _ in range(8)]
         self.pc = 0
@@ -32,25 +43,18 @@ class CPU:
         try:
             file_path = f"ls8/{seed_file}"
             with open(file_path) as file:
-
                 print(
                     f"{BColors.BOLD}{BColors.OK_GREEN}"
                     f"Loading program from {BColors.UNDERLINE}{BColors.OK_CYAN}{file_path}{BColors.END_}"
                 )
-
-                read_file = file.readlines()
-                program = [int(line.split()[0], 2) for line in read_file]
+                program = [int(line.split()[0], 2) for line in file.readlines()]
         except IOError:
-            print(f"{BColors.FAIL}An error occurred! Defaulting to 'print8.ls8'{BColors.END_}")
-            program = [
-                # From print8.ls8
-                0b10000010,  # LDI R0,8
-                0b00000000,
-                0b00001000,
-                0b01000111,  # PRN R0
-                0b00000000,
-                0b00000001,  # HLT
-            ]
+            print(
+                f"{BColors.FAIL}"
+                f"An error occurred! Defaulting to 'print8.ls8'"
+                f"{BColors.END_}"
+            )
+            program = DEFAULT_PROGRAM
 
         for instruction in program:
             self.ram[address] = instruction
@@ -90,24 +94,27 @@ class CPU:
     # --------------------------------------
     # OP HANDLERS
     # --------------------------------------
-    def handle_hlt(self, op_a, op_b):
+    def handle_hlt(self, **kwargs):
         # HALT
         print(f"{BColors.BOLD}{BColors.WARNING}HALTING{BColors.END_}")
         exit()
         return 0
 
-    def handle_ldi(self, op_a, op_b):
+    def handle_ldi(self, **kwargs):
         # LDI
+        op_a, op_b = kwargs["op_a"], kwargs["op_b"]
         self.reg[op_a] = op_b
         return 2
 
-    def handle_prn(self, op_a, op_b):
+    def handle_prn(self, **kwargs):
         # PRN
+        op_a = kwargs["op_a"]
         print(f"{self.reg[op_a]}")
         return 1
 
-    def handle_mul(self, op_a, op_b):
+    def handle_mul(self, **kwargs):
         # MUL
+        op_a, op_b = kwargs["op_a"], kwargs["op_b"]
         self.alu("MUL", op_a, op_b)
         return 2
 
@@ -119,16 +126,13 @@ class CPU:
         # 3. if the CPU is not halted by a HLT instruction, go to step 1
         while True:
             self.trace()
-
             # read the address in register `PC` and store that result in our instruction register
             ir = self.ram_read(self.pc)
-
             # read adjacent bytes in case the instruction requires them
             op_a = self.ram_read(self.pc + 1)
             op_b = self.ram_read(self.pc + 2)
 
-            # Then, depending on the value of the opcode, perform the actions
-            # needed for the instruction per the LS-8 spec.
+            # handle the instruction according to the LS-8 spec
             # update PC to point to the next instruction
             self.pc += 1 + self.branch_table[ir](op_a=op_a, op_b=op_b)
 
